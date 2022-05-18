cddfd4b360621cf265d7882c64f5a4ab1fe17ff1
Charawey-X <charawey@gmail.com>
2022-05-18 12:57:11 +0300

    add news class

---
+++ src/main/java/models/News.java
@@ -0,0 +1,16 @@
+package models;
+
+
+import java.util.Objects;
+
+public class News {
+    public int id;
+    public String content;
+    public String author;
+
+    private final static String DATABASETYPE = "News";
+
+    public News(String content, String author) {
+        this.content = content;
+        this.author = author;
+    }
